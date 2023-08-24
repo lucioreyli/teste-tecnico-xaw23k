152835ddef4896f1573d96d520cda737d77d3fe6
Lúcio Andrade <lucioandradejr@gmail.com>
2023-08-24 12:37:27 -0400

    style: component return

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,11 @@
 import { NavBar } from '@/components/NavBar';
 import type { FC, PropsWithChildren } from 'react';
 
-const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <>
-      <NavBar />
-      {children}
-    </>
-  );
-};
+const Layout: FC<PropsWithChildren> = ({ children }) => (
+  <>
+    <NavBar />
+    {children}
+  </>
+);
 
 export default Layout;
